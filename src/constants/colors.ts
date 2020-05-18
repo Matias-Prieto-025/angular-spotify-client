3e51efd9ca47f2d3343ba3ca0dc6f516b2bcab2b
Matias Prieto <matias.prieto@globant.com>
2020-05-18 16:14:15 -0300

    first commit

---
+++ src/constants/colors.ts
@@ -0,0 +1,5 @@
+export const BLACK: string = '#000';
+export const GRAY_1: string = '#191919';
+export const GRAY_2: string = '#333';
+export const GRAY_3: string = '#4d4d4d';
+export const GRAY_4: string = '#666';
